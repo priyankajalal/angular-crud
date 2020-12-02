294dd16baa685d1b2267aed7c935e55eb42cb4c1
priyankajalal <priyankajalaldit@gmail.com>
2020-12-02 12:32:50 -0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CrudOperationsComponent } from './shared/crud-operations/crud-operations.component';
+import { CategoriesComponent } from './shared/categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './shared/products/products.component';
+import { HeaderComponent } from './header/header.component';
+import { GroceryComponent } from './grocery/grocery.component';
+import { GroceryItemComponent } from './grocery-item/grocery-item.component';
+import { CartComponent } from './cart/cart.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
+export const routes: Routes = [
+  {path:'categories', component: CategoriesComponent},
+  {path:'products', component: ProductsComponent},
+  {path:'crud', component: CrudOperationsComponent},
+  // {path:'grocery', component: GroceryComponent},
+  {path:'shoppingCart', component: ShoppingCartComponent},
+  {path:'checkout', component: CheckoutComponent}
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudOperationsComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    HeaderComponent,
+    GroceryComponent,
+    GroceryItemComponent,
+    CartComponent,
+    ShoppingCartComponent,
+    CheckoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
